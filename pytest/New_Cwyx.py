933ca3e9842c9b6fe6b67f2c380d164a1be2cc43
AN <1090146450@qq.com>
2022-09-08 23:21:40 +0800

    边框

--- pytest/New_Cwyx.py
+++ pytest/New_Cwyx.py
@@ -15,13 +15,14 @@ class open_xlsx:
         CPU.range("B1").value = B1
         A2 = ["MIN", "=MIN(B5:B999999)", "=MIN(C5:C999999)", "=MIN(E5:E999999)"]
         CPU.range("A2").value = A2
-        A3 = ["MAN", "=MAN(B5:B999999)", "=MAN(C5:C999999)", "=MAN(D5:D999999)", "=MAN(E5:E999999)"]
+        A3 = ["MAN", "=MAX(B5:B999999)", "=MAX(C5:C999999)", "=MAX(D5:D999999)", "=MAX(E5:E999999)"]
         CPU.range("A3").value = A3
         B4 = ["TOTAL", "Cpu0", "Cpu1", "Cpu2"]
         CPU.range("B4").value = B4
         ae13 = CPU.range("A1:E3")
         ae13.color = (226, 239, 218)
-        ae13.api.Borders(9).LineStyle = 1
+        for b_id in range(7,13):
+            ae13.api.Borders(b_id).LineStyle = 1
         # 表01
 
-        book.save("C:/Users/Administrator/Desktop/DV/1.xlsx")
+        book.save("C:/Users/AN/Desktop/DV/1.xlsx")
